999782035b58c00bbfd6a06e2044b10aebcb2d94
Jim Heald <james.r.heald@gmail.com>
2017-08-21 16:45:42 -0700

    Documentation

--- binary_interface.h
+++ binary_interface.h
@@ -3,10 +3,26 @@
 
 #include "appdefs.h"
 
+/* Takes no arguments and returns 1 to indicate that there is a 
+ * devices mounted at /media/EVID_TARGET, or 0 otherwise */
 int target_is_mounted();
+
+/* Takes a pointer to blockdev string, and another to a string 
+ * that indicates what filesystem should be created. It returns a pointer to an
+ * array, terminated by -1, that represents the arguments for a subprocess to
+ * be created with */
 char **format_target_device(char *blockdev, char *format);
+
+/* Takes the name of a blockdev (e.g. "sda") and construct a command to mount 
+ * the associated path (e.g. "/dev/sda") at /media/EVID_TARGET. Will return
+ * null if the /media/EVID_TARGET is already mounted */
 char **mount_target_device(char *blockdev);
+
+/* To be implemented */
 int create_forensic_image(app_objects *globals);
+
+/* Take the name of a blockdev and writeblock it by executing `blockdev --setro
+ * /dev/<name>` TODO: check if it's writeblocked first */
 int writeblock_evidence_device(char *dev);
 
 #endif /* BINARY_INTERFACE_H */
