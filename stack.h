ead00ec2cbaa7fe2cda6796a1eae174ef007edbf
Jim Heald <james.r.heald@gmail.com>
2017-08-22 10:04:02 -0700

    Simple documentation

--- stack.h
+++ stack.h
@@ -8,9 +8,16 @@ typedef struct page_stack {
     int pages[MAX_PAGES];
 } page_stack;
 
+/* Creates a newly allocated stack */
 page_stack *new_stack();
+
+/* Put a page on the stack */
 void push_stack(page_stack *p, int page);
+
+/* Pop a page off the given stack and return its value */
 int pop_stack(page_stack *p);
+
+/* Return the value on the top of the stack */
 int peek_stack(page_stack *p);
 
 #endif /* STACK_H */
