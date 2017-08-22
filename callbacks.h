7f17a40bef5ab3a28af4d7579783b4e61b530eae
Jim Heald <james.r.heald@gmail.com>
2017-08-22 08:44:33 -0700

    Some documentation

--- callbacks.h
+++ callbacks.h
@@ -3,9 +3,14 @@
 
 #include<gtk/gtk.h>
 
+/* Many callbacks use this signature, so it made sense to make it a macro */
 #define NEW_CALLBACK(name) void name(GtkWidget *w, gpointer udata)
 
+/* Gets the previous page from the stack, navigate the notebook to
+ * it, and then get that page's title to set the header bar title */
 NEW_CALLBACK(notebook_previous_page);
+/* Unmounts the target drive and sets the evidence device back to RW, and then
+ * quits the app */
 NEW_CALLBACK(quit_app);
 
 NEW_CALLBACK(welcome_page_cb);
