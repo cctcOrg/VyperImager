627bfb18497742bb72c036054738b6a8bb57ae9c
Jim Heald <james.r.heald@gmail.com>
2017-08-22 08:44:16 -0700

    Documentation

--- devinfo.h
+++ devinfo.h
@@ -17,7 +17,15 @@ typedef struct Device {
     int  is_target;
 } Device;
 
+/* Will use parted to probe for all block devices and then construct Device
+ * structures for each one, storing the number of found block devices in the
+ * given num_blockdevs output parameter. Returns a newly allocated array of 
+ * Devices of the size indicated by the retuned num_blockdevs*/
+Device **get_blockdev_info(int *num_blockdevs);
+
+/* Uses a combination of blkid and parted to probe a given device for information
+ * so that the Device struct can be populated. Returns a newly allocated Device
+ * struct */
 Device *get_blockdev_struct(PedDevice *dev);
-Device **get_blockdev_info();
 
 #endif /* DEVINFO_H */
