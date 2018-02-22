3b81679e87fe77ca82e6250ef593fa94b96002bf
Jim Heald <james.r.heald@gmail.com>
2018-02-22 10:54:20 -0800

    Mostly copy over from old C, will update once device.cc is fully ported

--- src/blockdev_liststore.cc
+++ src/blockdev_liststore.cc
@@ -1,8 +1,69 @@
 #include "blockdev_liststore.h"
 
-BlockdevListStore::BlockdevListStore()
+BlockdevCols::BlockdevCols()
 {
-    ;
+    add(device_name);
+    add(device_model);
+    add(device_size);
+    add(numparts);
+    add(partlabels);
+    add(removable);
+
+    // Want this info to go somewhere else
+    add(is_target);
+    add(fspath);
+}
+
+BlockdevCols::~BlockdevCols()
+{
+}
+
+BlockdevListStore::BlockdevListStore(bool hide_internal, char *evid)
+{
+    set_column_types(cols);
+    blockdev_info = get_blockdev_info(&num_blockdevs);
+
+    for (int i=0; i<num_blockdevs; i++) {
+        partlabels = "";
+        dev = blockdev_info[i];
+ 
+        is_eviddev = ( evid != NULL  &&  (strcmp(evid, dev->name) == 0) ); 
+        /*if (evid != NULL) printf("%s\n", evid);*/
+        
+        for (int j = 0; j < dev->numparts; j++) {
+            label = dev->labels[j];
+            if (label != NULL) {
+                if (is_first) {
+                    partlabels = malloc(strlen(label)+1);
+                    strcpy(partlabels, label);
+                    is_first = false;
+                } 
+                else {
+                    /* Need to allocate 2 extra spaces for the ", " part and
+                     * and extra byte for the terminating null byte */
+                    partlabels = realloc(partlabels, 
+                            (strlen(partlabels) + strlen(label) + 3));
+                    strcat(partlabels, ", ");
+                    strcat(partlabels, label);
+                }
+            }
+        }
+        is_first = true;
+
+        if (hide_internal && (!dev->removable || is_eviddev))
+            continue;
+
+        auto row = *append();
+        row[cols.device_name] = dev->name;
+        row[cols.device_model] = dev->model;
+        row[cols.device_size] = dev->size;
+        row[cols.numparts] = dev->numparts;
+        row[cols.partlabels] = partlabels;
+        row[cols.removable] = (dev->removable) ? "Yes" : "No";
+        row[cols.is_target] = dev->is_target;
+        row[cols.fspath] = dev->fspath;
+    }
+
 }
 
 BlockdevListStore::~BlockdevListStore()
--- src/blockdev_liststore.h
+++ src/blockdev_liststore.h
@@ -1,11 +1,42 @@
+#include <gtkmm/treemodelcolumn.h>
 #include <gtkmm/liststore.h>
+#include <gtkmm/treeiter.h>
+
+#include "device.h"
+
+class BlockdevCols : public Gtk::TreeModel::ColumnRecord {
+    public:
+        BlockdevCols();
+        ~BlockdevCols();
+        Gtk::TreeModelColumn<Glib::ustring> device_name;
+        Gtk::TreeModelColumn<Glib::ustring> device_model;
+        Gtk::TreeModelColumn<Glib::ustring> device_size;
+        Gtk::TreeModelColumn<size_t>        numparts;
+        Gtk::TreeModelColumn<Glib::ustring> partlabels;
+        Gtk::TreeModelColumn<Glib::ustring> removable;
+
+        // These weren't shown in my original one, they should probably jlust be part of some object
+        Gtk::TreeModelColumn<size_t>        is_target;
+        Gtk::TreeModelColumn<Glib::ustring> fspath;
+};
 
 class BlockdevListStore : Gtk::ListStore
 {
     public:
-        BlockdevListStore();
+        BlockdevListStore(bool hide_internal, char *evid);
         ~BlockdevListStore();
 
     protected:
+        BlockdevCols cols;
+
         bool is_eviddev;
+        size_t num_blockdevs;
+        
+        Device **blockdev_info;
+        Device *dev;
+
+        // Changing all this to take advantage of C++
+        bool is_first;
+        char *label;
+        char *partlabels;
 };
